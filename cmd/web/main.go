c12c52192b8f02ada12cd054c8b0b215dc3a0440
vynquoc <vynquoc@gmail.com>
2023-08-21 20:40:40 +0700

    restruct project

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,14 +5,16 @@ import (
 	"net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from snippetbox"))
-}
-
 func main() {
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Println("Starting server on :4000")
 
 	err := http.ListenAndServe(":4000", mux)
+
 	log.Fatal(err)
 }
